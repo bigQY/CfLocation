959e2423b5503779ba60e4706b2dd3e1f9680ce9
bigqy <liuguoqing0394@qq.com>
2025-02-26 22:02:40 +0800

    更新Vite配置，添加自动导入Vue钩子和组件支持，升级依赖版本

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,31 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 自动导入vue中hook reactive ref等
+import AutoImport from 'unplugin-auto-import/vite'
+//自动导入ui-组件 比如说ant-design-vue  element-plus等
+import Components from 'unplugin-vue-components/vite'
+//ant-design-vue
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-import.d.ts',
+      //ant-design-vue
+      resolvers: [AntDesignVueResolver()]
+    }),
+    Components({
+      //ant-design-vue
+      resolvers: [
+        AntDesignVueResolver({ importStyle: false, resolveIcons: true })
+      ]
+    }),
+  ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
