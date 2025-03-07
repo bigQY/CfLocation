30faffe3c0841fd34c2f3d4ab004677ae4aa9e83
bigqy <liuguoqing0394@qq.com>
2025-03-07 11:47:23 +0800

    打包体积优化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-import Antd from 'ant-design-vue';
 import i18n from './lang/index.js'
 
 const app = createApp(App)
-app.use(Antd)
 app.use(i18n)
 app.mount('#app')
